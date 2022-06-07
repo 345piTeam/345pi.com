bda509ce0b1d2603bd0bef339bdf7a513160a1d6
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-06-07 16:10:06 -0400

    Update packages

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,26 +1,57 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { sanityClient } from '../../sanity';
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
 
-export default function News(props) {
-  return (
-    <div>
-      <Head>
-        <title>345pi - news</title>
-        <meta name="description" content="all posted 345pi news" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-
-      </main>
-    </div>
-  )
+interface Prop {
+	posts: Post[];
 }
 
-export const getServerSideProps = () => {
-  const query = `*[_type == "post"]`;
-  const result = sanityClient.fetch(query);
-  console.log(result);
+export default function News({ posts }: Prop) {
+	return (
+		<div>
+			<Head>
+				<title>345pi - news</title>
+				<meta name="description" content="all posted 345pi news" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				{posts.map((post: Post) => (
+					<Link key={post._id} href={"news/" + post.slug.current}>
+						<div className="bg-gradient-to-r from-cyan-500 to-blue-500 mx-auto text-center ease-in duration-300 transition-all">
+							<h1 className="text-7xl font-bold">{post.title}</h1>
+							<div className="">
+								<Image
+									alt={"Author's Profile Picture"}
+									src={urlFor(post.author.image).url()!}
+								></Image>
+								{post.author.name}
+							</div>
+						</div>
+					</Link>
+				))}
+			</main>
+		</div>
+	);
+}
 
-
-} 
+export const getServerSideProps = async () => {
+	const query = `*[_type == "post"]{
+		_id,
+		title,
+		author -> {
+			name, image
+		},
+		mainImage,
+		slug,
+	}`;
+	const posts = await sanityClient.fetch(query);
+	if (!posts) {
+		return { notFound: true };
+	} else {
+		return {
+			props: { posts },
+		};
+	}
+};
