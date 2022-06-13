df8081d057702065486ce9295d37f7e9e0ade93a
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-06-13 13:56:00 -0400

    Add placeholder whitepaper page

---
+++ pages/whitepaper.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Whitepaper = () => {
+	return (
+		<>
+			<Head>
+				<title>345pi - whitepaper</title>
+			</Head>
+			<div className="w-screen flex justify-center text-4xl">
+				<h1>Whitepaper</h1>
+			</div>
+		</>
+	);
+};
+
+export default Whitepaper;
